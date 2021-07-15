353d5f2354c068b637696236a30de154950e0826
4Azgin <doctorwhofr@protonmail.com>
2021-07-15 10:19:37 +0200

    modification de la micro api MythicMobManager.java rajout de la fonction -MythicMobManager.getItems()  -MythicMobManager.getMythicMobsItems()

--- src/fr/azgin/main/mythicmobs/MythicMobManager.java
+++ src/fr/azgin/main/mythicmobs/MythicMobManager.java
@@ -2,11 +2,13 @@ package fr.azgin.main.mythicmobs;
 
 import io.lumine.xikage.mythicmobs.MythicMobs;
 import io.lumine.xikage.mythicmobs.items.ItemManager;
-import io.lumine.xikage.mythicmobs.mobs.ActiveMob;
+import io.lumine.xikage.mythicmobs.items.MythicItem;
 import io.lumine.xikage.mythicmobs.mobs.MobManager;
 import io.lumine.xikage.mythicmobs.mobs.MythicMob;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+
+import java.util.Optional;
 
 public class MythicMobManager {
 
@@ -16,8 +18,11 @@ public class MythicMobManager {
         return this;
     }
 
-    public ItemStack getItems(String item_name){
-        return im.getItemStack(item_name);
+    public MythicItem getItems(String item_name){
+
+        Optional<MythicItem> mythicMobOptional = im.getItem(item_name);
+
+        return mythicMobOptional.orElse(null);
     }
 
 
