23ad7031e6f61bf68fce2cb5f19a3e4b628f0815
4Azgin <doctorwhofr@protonmail.com>
2021-07-15 10:20:57 +0200

    modification de la commande ClasseCommand ajout de la commande de reset quand la commande et effectué par la console et que la cible à un certain item elle reset et change par la classe préciser dans la commande.
    ---
    /classe DrAzgin mage
    
    va donc demander si la personne à déjà une classe l'item de reset;

--- src/fr/azgin/main/Commands/ClasseCommand.java
+++ src/fr/azgin/main/Commands/ClasseCommand.java
@@ -2,20 +2,32 @@ package fr.azgin.main.Commands;
 
 import fr.azgin.main.MainClass;
 import fr.azgin.main.core.loading.Model.NewPlayer;
+import fr.azgin.main.mythicmobs.MythicMobManager;
+import io.lumine.xikage.mythicmobs.adapters.AbstractItemStack;
+import io.lumine.xikage.mythicmobs.items.ItemManager;
+import io.lumine.xikage.mythicmobs.items.MythicItem;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.Server;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.checkerframework.checker.nullness.Opt;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class ClasseCommand implements CommandExecutor {
 
     MainClass main = MainClass.getInstance();
-
+    MythicMobManager mmm = new MythicMobManager();
 
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
@@ -39,8 +51,20 @@ public class ClasseCommand implements CommandExecutor {
 
                     return true;
                 } else {
-                    np.get_p().sendMessage("vous avez déjà une classe, vous pouvez changez de classe auprès du maitre de stage.");
+                    if(commandSender instanceof ConsoleCommandSender){
+                        ItemStack crow = mmm.getMythicMobsItems("KingsCrown");
 
+                        Inventory inventory = np.get_p().getInventory();
+                        if(inventory.contains(crow)){
+                            inventory.remove(crow);
+                            np.setClasse(_classe);
+                            np.get_p().sendMessage("Vous venez de changez de classe par: " + _classe);
+                        }
+
+
+                    } else {
+                        np.get_p().sendMessage("vous avez déjà une classe, vous pouvez changez de classe auprès du maitre de stage.");
+                    }
                     return true;
                 }
             }
