f3ceec30ad941c073873cdec5a1ca1199dace286
4Azgin <doctorwhofr@protonmail.com>
2021-08-01 12:42:19 +0200

    fixing mythicmob api

--- src/fr/azgin/main/mythicmobs/MythicMobManager.java
+++ src/fr/azgin/main/mythicmobs/MythicMobManager.java
@@ -1,11 +1,8 @@
 package fr.azgin.main.mythicmobs;
 
 import io.lumine.xikage.mythicmobs.MythicMobs;
-import io.lumine.xikage.mythicmobs.adapters.AbstractLocation;
-import io.lumine.xikage.mythicmobs.adapters.AbstractWorld;
 import io.lumine.xikage.mythicmobs.items.ItemManager;
 import io.lumine.xikage.mythicmobs.items.MythicItem;
-import io.lumine.xikage.mythicmobs.mobs.ActiveMob;
 import io.lumine.xikage.mythicmobs.mobs.MobManager;
 import io.lumine.xikage.mythicmobs.mobs.MythicMob;
 import org.bukkit.entity.Player;
@@ -15,28 +12,25 @@ import java.util.Optional;
 
 public class MythicMobManager {
 
-    ItemManager im = new ItemManager(MythicMobs.inst());
-    MobManager  m = new MobManager(MythicMobs.inst());
-
     public MythicMobManager MythicMobManager(){
         return this;
     }
 
-    public MythicItem getItems(String item_name){
+    public Optional<MythicItem> getItems(String item_name){
+        ItemManager im = MythicMobs.inst().getItemManager();
 
-        Optional<MythicItem> mythicMobOptional = im.getItem(item_name);
 
-        return mythicMobOptional.orElse(null);
+        return im.getItem(item_name);
     }
 
     public ItemStack getMythicMobsItems(String name){
-        ItemManager im = new ItemManager(MythicMobs.inst());
+        ItemManager im = MythicMobs.inst().getItemManager();
 
         return im.getItemStack(name);
     }
 
     public void giveMythicMobsItems(String name, Player p, double amount){
-        ItemManager im = new ItemManager(MythicMobs.inst());
+        ItemManager im = MythicMobs.inst().getItemManager();
         ItemStack imItemStack = im.getItemStack(name);
 
         for (int i = 0; i < amount; i++) {
@@ -45,7 +39,7 @@ public class MythicMobManager {
     }
 
     public MythicMob getMobs(String name){
-        MobManager mb = new MobManager(MythicMobs.inst());
+        MobManager mb = MythicMobs.inst().getMobManager();
 
         return mb.getMythicMob(name);
     }
